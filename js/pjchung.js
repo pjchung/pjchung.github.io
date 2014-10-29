df35026a9554830ca7259dcc3c3ebe2ba78190ab
jjinho <paul.joshua.chung@gmail.com>
2014-10-29 17:34:15 +0000

    Refactored Search and Topbar under PJChung.JS

--- js/pjchung.js
+++ js/pjchung.js
@@ -1,4 +1,19 @@
-angular.module("pjchungApp", ['mm.foundation']);
-var topBarCtrl = function ($scope){
-  
+var pjchungApp = angular.module("pjchungApp", ['mm.foundation']);
+
+var TopBarCtrl = function ($scope){  
 };
+
+pjchungApp.config([
+  // Use '{(' and ')}' to avoid conflict with liquid tags
+  '$interpolateProvider', function($interpolateProvider) {
+    return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+  }
+]);
+
+// Gets the search.json file and attaches it to SearchCtrl's search model
+pjchungApp.controller('SearchCtrl', function($scope, $http) {
+  $http.get('/search.json')
+  .then(function(results) {
+    $scope.search = results.data;
+  });
+});
